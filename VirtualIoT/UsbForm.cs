eaa1777a3fb7d986e9d55db2b937576fce7250fa
Ewolly <ewollypop@gmail.com>
2017-05-25 20:08:18 +1200

    anotherone

--- VirtualIoT/UsbForm.cs
+++ VirtualIoT/UsbForm.cs
@@ -1,48 +1,38 @@
 ﻿using MessagePack;
 using Gma.System.MouseKeyHook;
-using MessagePack;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using VirtualIoT.Properties;
+using Newtonsoft.Json;
 
 namespace VirtualIoT
 {
     public partial class UsbForm : Form
     {
         private IKeyboardMouseEvents _globalHook;
-        SslStream _sslStream;
+        SslStream _sslStream = null;
         DeviceInfo _device;
-        private TcpListener _server;
-        private IPAddress _ip = IPAddress.Parse("192.168.0.137");
-        private int _port = 12345;
-        private TcpClient _tcpClient = new TcpClient();
         private SslStream _sslClient;
         public X509Certificate2 _cert = new X509Certificate2(Resources.server, "IoTBox");
         public UsbData _usbData;
-        private System.Windows.Forms.Timer _usbTimer;
+        private Timer _usbTimer;
         private bool _send;
+        private Timer _aliveTimer;
+        private Timer _timer;
 
         public UsbForm(DeviceInfo device)
         {
             InitializeComponent();
-            _server = new TcpListener(_ip, _port);
-            _server.Start();
             _device = device;
 
-            _usbTimer = new System.Windows.Forms.Timer();
+            _usbTimer = new Timer();
             _usbTimer.Interval = 5;
             _usbTimer.Tick += SendData;
         }
@@ -59,13 +49,31 @@ namespace VirtualIoT
 
         private void USB_Load(object sender, EventArgs e)
         {
-            _globalHook = Hook.GlobalEvents();
+
             _sslStream = _device.CreateSocket();
             if (_sslStream == null)
             {
                 MessageBox.Show("Connection Failed");
                 this.Close();
             }
+
+            //start timer
+            _timer = new Timer()
+            {
+                Interval = 200
+            };
+            _timer.Tick += UpdateTimer;
+            _timer.Start();
+
+            // timer for the keep alive
+            _aliveTimer = new Timer()
+            {
+                Interval = 2000
+            };
+            _aliveTimer.Tick += aliveTimer;
+            _aliveTimer.Start();
+
+            _globalHook = Hook.GlobalEvents();
         }
 
         private void currentHsb_Scroll(object sender, ScrollEventArgs e)
@@ -79,17 +87,6 @@ namespace VirtualIoT
 
         }
 
-        private void clientsBtn_Click(object sender, EventArgs e)
-        {
-            _tcpClient = new TcpClient();
-            textBox1.AppendText("Waiting for new Client");
-            _tcpClient = _server.AcceptTcpClient();
-            _sslClient = new SslStream(_tcpClient.GetStream(), false);
-            _sslClient.AuthenticateAsServer(_cert, false, SslProtocols.Tls, true);
-            textBox1.AppendText("Connected new client: " + _tcpClient.Client.RemoteEndPoint);
-            SubscribeEvents();
-        }
-        
         public void SubscribeEvents()
         {
             _usbData = new UsbData()
@@ -143,5 +140,85 @@ namespace VirtualIoT
             _usbData.key = (byte)e.KeyChar;
             _send = true;
         }
+
+        private void aliveTimer(object sender, EventArgs e)
+        {
+            _device.SendKeepalive(_sslStream, currentHsb.Value);
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+
+        private void UpdateTimer(object sender, EventArgs e)
+        {
+            var old_timeout = _sslStream.ReadTimeout;
+            _sslStream.ReadTimeout = 10;
+            var buffer = new byte[128];
+            ResultObject result = null;
+            try
+            {
+                _sslStream.Read(buffer, 0, 128);
+                result = JsonConvert.DeserializeObject<ResultObject>(
+                        Encoding.UTF8.GetString(buffer));
+            }
+            catch
+            {
+                return;
+            }
+
+
+            if (result == null)
+                return;
+
+            if (result.server != null)
+            {
+                if (_sslClient != null)
+                {
+                    // put ssl tcp server start code here
+                    // equivalent of start button
+
+                    var server = new TcpListener(IPAddress.Any, 0);
+                    server.Start();
+                    var port = ((IPEndPoint)server.LocalEndpoint).Port;
+                    var tcpClient = new TcpClient();
+                    textBox1.AppendText("Waiting for new Client");
+                    tcpClient = server.AcceptTcpClient();
+                    _sslClient = new SslStream(tcpClient.GetStream(), false);
+                    _sslClient.AuthenticateAsServer(_cert, false, SslProtocols.Tls, true);
+                    textBox1.AppendText("Connected new client: " + tcpClient.Client.RemoteEndPoint);
+                    SubscribeEvents();
+
+                    _device.ConvertAndSend(_sslClient, new ResponseObject
+                    {
+                        response = "server_setup",
+                        kwargs = new Dictionary<string, object>
+                    {
+                        { "ip" , GetLocalIPAddress() },
+                        { "port" , port }
+                    }
+                    });
+                }
+            }
+            else if (result.info != null)
+            {
+                statusLbl.Text = "Info: " + result.info;
+            }
+            else if (result.error != null)
+            {
+                statusLbl.Text = "Error: " + result.error;
+            }
+
+        }
+
     }
 }
