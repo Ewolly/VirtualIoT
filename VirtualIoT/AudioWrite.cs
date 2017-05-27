f358b79a1a92a07e0aeb165805fa0e249c962d94
Jamie Harvey <jharvey@duality.co.nz>
2017-05-27 14:21:50 +1200

    audiowrtei

--- VirtualIoT/AudioWrite.cs
+++ VirtualIoT/AudioWrite.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace VirtualIoT
 {
@@ -15,18 +16,49 @@ namespace VirtualIoT
     {
         private bool _isPlayback = false;
 
-        public void StartPlayback(FixedSizedQueue<byte[]> queue)
+        private Stream ms = new MemoryStream();
+        public void StreamMp3(object sslStream)
+        {
+            Stream stream = (Stream)sslStream;
+            new Thread(delegate (object o)
+            {
+                byte[] buffer = new byte[65536]; // 64KB chunks
+                int read;
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    var pos = ms.Position;
+                    ms.Position = ms.Length;
+                    ms.Write(buffer, 0, read);
+                    ms.Position = pos;
+                }
+            }).Start();
+
+            // Pre-buffering some data to allow NAudio to start playing
+            while (ms.Length < 65536 * 10)
+                Thread.Sleep(1000);
+
+            ms.Position = 0;
+            using (WaveStream blockAlignedStream = new BlockAlignReductionStream(WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(ms))))
+            {
+                using (WaveOut waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
+                {
+                    waveOut.Init(blockAlignedStream);
+                    waveOut.Play();
+                    while (waveOut.PlaybackState == PlaybackState.Playing)
+                    {
+                        System.Threading.Thread.Sleep(100);
+                    }
+                }
+            }
+        }
+
+        public void StartPlayback(SslStream sslStream)
         {
             var waveOut = new WaveOutEvent();
             if (_isPlayback)
                 return;
 
-            using (Mp3FileReader mp3 = new Mp3FileReader(new QueueStream(queue)))
-            {
-                waveOut.Init(mp3);
-                waveOut.Play();
-            }
+            ThreadPool.QueueUserWorkItem(StreamMp3, sslStream);
         }
-
     }
 }
